3e180af140a803bfd20489b5ebbfbd48b13d473d
franvasquez9 <89528074+franvasquez9@users.noreply.github.com>
2022-06-27 15:39:43 -0500

    ejercicio 2 JS

---
+++ app.js
@@ -0,0 +1,28 @@
+
+//con lista
+const lista = ["Francisco", 31, true, Date("october 14 1990"),
+
+libro = {
+    titulo: "Morir otra vez",
+    autor: "Alberto Martinez Sanchez",
+    fecha: "01-01-2000",
+    url: "https://www.amazon.com/-/es/Alberto-Mart%C3%ADnez-S%C3%A1nchez-ebook/dp/B09LSS52Y4/ref=sr_1_1?__mk_es_US=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=3LFU1K3HFGQL&keywords=libro+de+terror&qid=1656360980&sprefix=libro+de+terror%2Caps%2C174&sr=8-1",
+}
+]
+console.log(lista);
+
+//no entendi bien el ejercicio, en vez de lista lo hice con objetos
+
+const persona ={
+    nombre: "Francisco",
+    edad: 31,
+    programador: true,
+    fecha: new Date("october 14 1990"),
+    libro: {
+        titulo: "Morir otra vez",
+        autor: "Alberto Martinez Sanchez",
+        fecha: "01-01-2000",
+        url: "https://www.amazon.com/-/es/Alberto-Mart%C3%ADnez-S%C3%A1nchez-ebook/dp/B09LSS52Y4/ref=sr_1_1?__mk_es_US=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=3LFU1K3HFGQL&keywords=libro+de+terror&qid=1656360980&sprefix=libro+de+terror%2Caps%2C174&sr=8-1",
+    }
+}
+console.log(persona)
